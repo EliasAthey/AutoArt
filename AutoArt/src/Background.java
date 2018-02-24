e2b13eb28e7125178e499a35b3217ffbf2f054b7
Elias A <athey.elias@gmail.com>
2018-02-24 13:21:33 -0700

    Implemented gradient for the background fromt he primary to the secondary color. Uses a random rotation to add variation

--- AutoArt/src/Background.java
+++ AutoArt/src/Background.java
@@ -2,6 +2,8 @@
  * AutoArt is licensed under the GPL 3.0. Written by Elias Athey and Tia Smith.
  */
 
+import static processing.core.PConstants.PI;
+
 /**
  * The Background class paints the background of the image.
  */
@@ -21,16 +23,48 @@ public class Background {
     /**
      * How strong/steep the gradient is, defaults to 50%
      */
-    private float intensity = 0.5f;
+    private float intensity = 0.0f;
 
     /**
      * Paints the background
      */
     public void paintBackground(Painter painter){
-        painter.background(0,0,0);
-        painter.fill(this.firstColor[0], this.firstColor[1], this.firstColor[2],255 - this.value);
-        painter.rect(-1,-1,painter.getCanvasWidth() + 1, painter.getCanvasHeight() + 1);
+	    // rotate the canvas before drawing the gradient, use random radian [-PI/2,PI/2]
+	    float rotation = (float)(Math.random() * PI) - PI/2;
+	    int intensityDifference = (int)((this.intensity - 0.5f) * painter.getCanvasHeight());
+	    painter.rotate(rotation);
+        this.setGradient(
+        		painter,
+		        (int)(1.5f * -painter.getCanvasWidth()),
+		        (int)(1.5f * -painter.getCanvasHeight() + intensityDifference),
+		        4*painter.getCanvasWidth(),
+		        4*painter.getCanvasHeight());
+
+	    // return the canvas to its original orientation
+	    painter.rotate(-rotation);
     }
+
+	/**
+	 * Paints a gradient for the background
+	 * @param painter the painter
+	 * @param x the starting x coord
+	 * @param y the starting y coord
+	 * @param w the width
+	 * @param h the height
+	 */
+	private void setGradient(Painter painter, int x, int y, float w, float h) {
+		painter.noFill();
+
+		for (float iY = y, iX = x; iY <= y+h && iX <= x+w; iY=iY+0.1f, iX=iX+0.1f) {
+			float interY = Painter.map(iY, y, y+h, 0, 1);
+			painter.stroke(
+					painter.lerpColor(
+							painter.color(this.firstColor[0],this.firstColor[1],this.firstColor[2]),
+							painter.color(this.secondColor[0],this.secondColor[1],this.secondColor[2]),
+							interY));
+			painter.line(x, iY, x+w, iY);
+		}
+	}
 
 	/**
 	 * Sets the primary color
